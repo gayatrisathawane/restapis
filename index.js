90c4f70451cb6cb729f02a754bd85c359dedfbd7
gayatrisathawane <gayatripsathawane@gmail.com>
2023-11-27 11:59:05 +0530

    basic server designed

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+const PORT  = process.env.PORT || 8080
+
+
+
+app.listen(PORT ,()=>{
+    console.log(`server is running on port ${PORT}`)
+})
+
